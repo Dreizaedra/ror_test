05d46b3c9d0ff29b03179a88efe1a5ea20e91f77
Louis ARNAULT <l.arnault@kami.fr>
2025-08-05 14:21:40 +0200

    feat: add product display & product form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "/products", to: "products#index", as: "products"
+  post "/products", to: "products#create", as: "create_product"
+  get "/products/new", to: "products#new", as: "new_product"
+
+  get "/products/:id", to: "products#show", as: "product"
+
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
