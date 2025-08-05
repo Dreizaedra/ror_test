acef4772d67335cb1e72436e0b62827617bde5b5
Louis ARNAULT <l.arnault@kami.fr>
2025-08-05 14:35:36 +0200

    feat: add product edit form

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/products", to: "products#index", as: "products"
   post "/products", to: "products#create", as: "create_product"
   get "/products/new", to: "products#new", as: "new_product"
-
+  get "/products/:id/edit", to: "products#edit", as: "edit_product"
+  patch "/products/:id", to: "products#update", as: "update_product"
   get "/products/:id", to: "products#show", as: "product"
-
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
