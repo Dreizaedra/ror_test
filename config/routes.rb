992aa91048cd78e3426f234afed1efe3e14949bc
Louis ARNAULT <l.arnault@kami.fr>
2025-08-05 14:47:36 +0200

    feat: add delete product route and logic

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post "/products", to: "products#create", as: "create_product"
   get "/products/new", to: "products#new", as: "new_product"
   get "/products/:id/edit", to: "products#edit", as: "edit_product"
+  delete "/products/:id", to: "products#destroy", as: "destroy_product"
   patch "/products/:id", to: "products#update", as: "update_product"
   get "/products/:id", to: "products#show", as: "product"
 end
